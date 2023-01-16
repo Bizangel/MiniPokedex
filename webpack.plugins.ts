b5a0c1acd525b44f34d80a0d6cac4cea9f46cade
Luis De Ávila <luis.deavila@urosario.edu.co>
2023-01-16 21:33:43 +0000

    Added compatibility with full URLs + webpack assets.

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,5 +1,9 @@
 import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 
+// eslint-disable-next-line import/default
+import CopyPlugin from "copy-webpack-plugin"
+
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
@@ -7,4 +11,9 @@ export const plugins = [
   new ForkTsCheckerWebpackPlugin({
     logger: 'webpack-infrastructure',
   }),
+  new CopyPlugin({
+    patterns: [
+      { from: "assets", to: "assets" } // this will end up in .webpack/renderer/assets
+    ]
+  })
 ];
